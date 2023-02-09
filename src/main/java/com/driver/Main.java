cfc438bd97112798974f0becaf5a352105206e91
Arpit <arpitjain7254@gmail.com>
2023-02-09 12:01:21 +0530

    arpit jain

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obja=new RWOnly();
+        obja.setName("craj");
+        System.out.println(obja.getName());
+    }
   
 }
